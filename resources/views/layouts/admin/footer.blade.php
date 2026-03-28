5e215eef2885311885748b3a184f052824363481
dev-charles <charles.sagitariawan@sqiva.com>
2026-03-28 11:43:49 +0700

    remove footer watermark

--- resources/views/layouts/admin/footer.blade.php
+++ resources/views/layouts/admin/footer.blade.php
@@ -3,5 +3,4 @@
     <script>
         document.write(new Date().getFullYear())
     </script>
-    <a href="http://ptusi.co.id/" target="_blank" rel="noopener noreferrer">{{ Str::upper(config('app.name', 'United Shiping Indonesia')) }}</a>. All Rights Reserved.
 </footer>
